ee11445c4c48d32696e9eb5cd588f385ccaa2aa8
Vladimir Marohovsky <vladimir.marohovsky@gmail.com>
2023-02-22 22:25:54 +0200

    Initializing project

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className=' bg-hunter-green shadow-sm'>
+            <div className=' max-w-7xl mx-auto text-white py-6 px-4'>
+                <h1>Home</h1>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
