50c96a22b91f8e65ba9bdd6e3a0bf9c39d2c33b1
Bohdane <berlo1113@gmail.com>
2023-02-24 13:31:04 +0200

    modified:   src/components/deliveyRender/deliveryRender.tsx 	modified:   src/pages/_document.tsx 	new file:   src/pages/about.tsx 	modified:   src/styles/globals.css

--- src/components/deliveyRender/deliveryRender.tsx
+++ src/components/deliveyRender/deliveryRender.tsx
@@ -7,11 +7,10 @@ import manualPoshta_icon from '../../../public/mapPoint.png';
 import { useEffect, useState } from "react";
 
 const DeliveryRender = () =>{
-// Обработка кликов
+
     const[vision,setVision] = useState('');
-    useEffect(()=>{
-        console.log(vision)
-    },[vision]);
+
+// -------------------------- То что будет выводиться при клике- на 85 строке--------------------------------
 
             const map =(<div id="down" className="map mt-[20px] " >
                         <iframe className="w-[1400px] h-[800px] border-4 border-cyan-800" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1813.9645622900193!2d36.15768552458085!3d49.98636062173905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4127a1bfc4caf2a5%3A0x1db2782fb68bcdd2!2z0YPQuy4g0KHQtdGA0LPQtdGPINCR0L7RgNC30LXQvdC60L4sIDgsINCl0LDRgNGM0LrQvtCyLCDQpdCw0YDRjNC60L7QstGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCA2MTAwMA!5e0!3m2!1sru!2sua!4v1677152052871!5m2!1sru!2sua"    loading="lazy" ></iframe>
@@ -54,17 +53,14 @@ const DeliveryRender = () =>{
 
                 </div>
             );
-              
-                
-                
-               
-            
+//--------------------Верстка............................             
+                        
     return(
-    <div className=' shadow-sm'> 
+    <div className=' shadow-sm mt-[40px] mb-[20px]'> 
         <div className=' max-w-7xl mx-auto '>
             
-            <h2 className="  max-w-[600px] h-[60px] border-b-2 ml-auto mr-auto mt-[20px] text-[60px] text-center max-[600px]:hidden">Доставка та оплата</h2>
-            <h3 className=" max-w-80 mt-[20px] ml-auto mr-auto pb-[20px] text-[20px] text-center border-b-2">- Варіанти доставки -</h3>
+            <h2 className="  max-w-[600px] h-[60px] border-b-2 ml-auto mr-auto mt-[20px] text-[60px] text-center max-[600px]:hidden font-[Lobster]">Доставка та оплата</h2>
+            <h3 className=" max-w-80 mt-[20px] ml-auto mr-auto pb-[20px] text-[20px] text-center border-b-2 font-[Shantell Sans]">- Варіанти доставки -</h3>
 
             <ul className="mt-[20px] flex flex-wrap justify-center items-center gap-4 ">
                <li onClick={(e)=>setVision('novaPoshta')}>
@@ -82,11 +78,11 @@ const DeliveryRender = () =>{
                 </a>
                </li>
             </ul>
-                    <h4 className="w-[200px] mt-[20px] ml-auto mr-auto text-center">- Самовивіз -</h4>
+                    <h4 className="w-[200px] mt-[20px]  ml-auto mr-auto text-center">- Самовивіз -</h4>
                     <button onClick={(e)=>setVision('map')} className="w-[220px] h-[200px] mt-[20px] ml-auto mr-auto pl-[20px] border-2 rounded-[20px] hover:bg-slate-200  block ">
                         <Image src={manualPoshta_icon} alt='NovaPoshta' placeholder="blur" width={180} height={180}/>
                     </button>
-                    
+{/* ------------------------------Варианты вывода верстки 13 строка------------------------------------------  */}
                     {(vision == 'map') ? map : (vision == 'novaPoshta') ? novaPsohta : (vision == 'ukrPoshta') ? ukrPoshta : null }
                    
         </div>
