b98b424d2d40064b3bdb83a6014d1c258ff811b0
Bohdane <berlo1113@gmail.com>
2023-03-11 00:07:27 +0200

    modified:   src/components/ItemCart/ItemCart.tsx

--- src/components/ItemCart/ItemCart.tsx
+++ src/components/ItemCart/ItemCart.tsx
@@ -16,12 +16,16 @@ const ItemCart = () =>{
     const orderDispatch = () =>{
         console.log(store.getState())
     }
+    let fullOrderPrice:number = 0;
+    ArrItems.forEach(element => {
+        fullOrderPrice += element.itemPrice
+    });
     return(
         <div className="w-[600px] min-h-[200px] p-2 mx-auto border-2 bg-slate-200 bg-opacity-75 absolute z-[1] top-[110px] right-0">
             <ul className="flex flex-col gap-2">
                 {allItems.length == 0 ? <p>Корзина пуста</p>: allItems} 
             </ul>
-            {store.getState().shopingArr.length != 0 ? <button className="w-[200px] h-[50px] mx-auto mt-2 bg-green-900 block" onClick={()=> orderDispatch()}>Придбати</button> : null}
+            {store.getState().shopingArr.length != 0 ? <button className="w-[200px] h-[50px] mx-auto mt-2 bg-green-900 block" onClick={()=> orderDispatch()}>{fullOrderPrice} грн</button> : null}
             
         </div>
     )
