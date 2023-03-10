f2404fea09e394c36e0e35856ccf073dbcc8d70d
Bohdane <berlo1113@gmail.com>
2023-03-11 00:44:54 +0200

    modified:   src/components/ItemCart/ItemCart.tsx

--- src/components/ItemCart/ItemCart.tsx
+++ src/components/ItemCart/ItemCart.tsx
@@ -23,7 +23,7 @@ const ItemCart = () =>{
     return(
         <div className="w-[600px] min-h-[200px] p-2 mx-auto border-2 bg-slate-200 bg-opacity-75 absolute z-[1] top-[110px] right-0">
             <ul className="flex flex-col gap-2">
-                {allItems.length == 0 ? <p>Корзина пуста</p>: allItems} 
+                {allItems.length == 0 ?  <div><p className="w-[300px] mx-auto mt-5 text-[30px]">Поки ще тут пусто</p><img className="mx-auto" src="https://i.gifer.com/7VE.gif"/></div>: allItems} 
             </ul>
             {store.getState().shopingArr.length != 0 ? <button className="w-[200px] h-[50px] mx-auto mt-2 bg-green-900 block" onClick={()=> orderDispatch()}>{fullOrderPrice} грн</button> : null}
             
